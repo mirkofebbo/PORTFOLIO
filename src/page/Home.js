08a2dbacff3e95047f84b2e05e1ce3ee86e14ef3
Mirko Febbo <56055818+mirkofebbo@users.noreply.github.com>
2023-08-12 17:49:16 +0100

    reactive home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Typography, Card, CardContent, CardMedia, Box, Divider } from "@mui/material";
+import { Typography, Card, CardContent, CardMedia, Box, Divider, useMediaQuery } from "@mui/material";
 import projectData from "../data/projectData.json"; // Make sure to import the correct path to your JSON file
 import { useNavigate } from 'react-router-dom';
-
+import { useTheme } from '@mui/material/styles';
 const Home = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const navigate = useNavigate();
+
   // Function to get 3 random projects
   const getRandomProjects = (projects, count = 3) => {
     const shuffled = [...projects].sort(() => 0.5 - Math.random());
@@ -15,21 +18,36 @@ const Home = () => {
 
   return (
     <div>
-      <Typography variant="h4">
-        Highlights
-      </Typography>
-      <Divider sx={{ marginY: 2, }} />
+      <Typography variant="h4">Highlights</Typography>
+      <Divider sx={{ marginY: 2 }} />
       <Typography variant="body2">
-        I am a human, artist and researcher,  exploring the intersection of art, technology, and geopolitics. With a background in sculpture from Concordia University in Montreal, in addition to holding a data science certificate from HarvardX, and a masters in computational arts from Goldsmiths University. I am particularly interested in the ways in which data visualisation and kinetic art can be used to convey complex information in a more engaging and interactive way. I frequently incorporate digital media and fabrication techniques, into my artistic practice.
+        I am a human, artist and researcher, exploring...
       </Typography>
-      <Divider sx={{ marginY: 2, }} />
-      <Box sx={{ display: "flex", justifyContent: "space-between", flexWrap: "wrap", marginTop: 3 }}>
+      <Divider sx={{ marginY: 2 }} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: isMobile ? 'column' : 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}
+      >
         {randomProjects.map((project, id) => (
-          <Card key={id} sx={{ width: "30%", margin: 1 }} onClick={() => navigate(`/Project/${project.id}`)}
+          <Card
+            key={id}
+            sx={{
+              width: isMobile ? '100%' : '30%',
+              margin: isMobile ? '10px 0' : '10px',
+            }}
+            onClick={() => navigate(`/Project/${project.id}`)}
           >
             <CardMedia
               component="img"
-              sx={{ height: 300, width: 300 }}
+              sx={{
+                height: isMobile ? 200 : 300,
+                width: '100%',
+                objectFit: 'cover',
+              }}
               image={project.mediaUrl[0]}
               alt={`Image for ${project.title}`}
             />
@@ -38,9 +56,7 @@ const Home = () => {
               <Typography variant="body1">
                 [{project.keywords.join(', ')}]
               </Typography>
-              <Typography variant="body2">
-                {`${project.oneLiner}`}
-              </Typography>
+              <Typography variant="body2">{`${project.oneLiner}`}</Typography>
             </CardContent>
           </Card>
         ))}
