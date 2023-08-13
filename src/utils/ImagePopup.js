78a2bb039c684de5bd7a833ef82d7b868d4e2519
Mirko Febbo <56055818+mirkofebbo@users.noreply.github.com>
2023-08-13 21:25:59 +0100

    add close button to image popup

--- src/utils/ImagePopup.js
+++ src/utils/ImagePopup.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogTitle, Typography } from '@mui/material';
-
+import { Dialog, DialogContent, DialogTitle, Typography, IconButton } from '@mui/material';
+import CancelIcon from '@mui/icons-material/Cancel';
 const ImagePopup = ({ open, onClose, mediaUrl, description }) => {
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle>Image Details</DialogTitle>
+      <DialogTitle>
+        Image Details
+        <IconButton
+          edge="end"
+          color="inherit"
+          onClick={onClose}
+          aria-label="close"
+          sx={{ position: 'absolute', right: 8, top: 8 }}
+        >
+          <CancelIcon />
+        </IconButton>
+      </DialogTitle>
       <DialogContent>
         {mediaUrl && (
           <img
